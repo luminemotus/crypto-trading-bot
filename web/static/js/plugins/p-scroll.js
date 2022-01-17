3eb1f6c0fe16ff071d7dda3d3e5f035bf04a8fe6
joseluis <panel@hotelgest.com>
2022-01-17 11:10:12 +0100

    test. version

---
+++ web/static/js/plugins/p-scroll.js
@@ -0,0 +1,21 @@
+(function($) {
+	
+	const ps2 = new PerfectScrollbar('.chat-scroll', {
+	  useBothWheelAxes:true,
+	  suppressScrollX:true,
+	});
+	const ps3 = new PerfectScrollbar('.Notification-scroll', {
+	  useBothWheelAxes:true,
+	  suppressScrollX:true,
+	});
+	const ps31 = new PerfectScrollbar('.text-scroll', {
+	  useBothWheelAxes:true,
+	  suppressScrollX:true,
+	});
+	const ps32 = new PerfectScrollbar('.notify-scroll', {
+	  useBothWheelAxes:true,
+	  suppressScrollX:true,
+	});
+	
+	
+})(jQuery);
---
+++ web/static/js/plugins/sticky.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){var o=$(".sticky"),s="sticky-pin",n=o.offset().top;function t(){$(this).scrollTop()>=n?o.addClass(s):o.removeClass(s)}t(),$(window).scroll((function(){t()}))}));
